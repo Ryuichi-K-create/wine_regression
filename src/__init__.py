a4650ecec33fdaacc11b8cb167a90a5beaf4b69c
Ryuichi-K-create <ryu-konishi@stu.kanazawa-u.ac.jp>
2025-09-21 10:46:43 +0900

    init_pyの更新

---
+++ src/__init__.py
@@ -0,0 +1,26 @@
+# src/__init__.py
+"""
+Wine Quality Regression Project
+
+A machine learning project for predicting wine quality using neural networks.
+"""
+
+__version__ = "1.0.0"
+
+from .config import Config
+from .utils import (
+    load_dataframe,
+    compute_regression_metrics,
+    set_seed,
+    make_run_dir
+)
+from .models import MLPRegressor
+
+__all__ = [
+    'Config',
+    'load_dataframe', 
+    'compute_regression_metrics',
+    'set_seed',
+    'make_run_dir',
+    'MLPRegressor'
+]
